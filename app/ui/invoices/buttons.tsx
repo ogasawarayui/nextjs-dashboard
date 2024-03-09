da559f9fb5d7fddf87ea2665d5be3f1fa0978e4c
ogasawarayui <ogasawara.yui@outlook.com>
2024-03-09 23:05:08 +0900

    app

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,3 +1,4 @@
+import { deleteInvoice } from '@/app/lib/actions';
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
@@ -25,9 +26,10 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
   return (
     <Link
-    href={`/dashboard/invoices/${id}/edit`}
+    <form action={deleteInvoiceWithId}>
       className="rounded-md border p-2 hover:bg-gray-100">
         <TrashIcon className="w-5" />
     </Link>
